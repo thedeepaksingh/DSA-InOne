ed9fcfc4d821b4f48c0b1f3a6e52c365e2e663e2
the_ds <128127664+thedeepaksingh@users.noreply.github.com>
2025-08-25 22:20:18 +0530

    Initial commit with core folder

--- Problems/Easy/2806LC_Fn.js
+++
@@ -1,8 +0,0 @@
-// 2806. Account Balance After Rounded Purchase
-
-const accountBalanceAfterPurchase = function (purchaseAmount) {
-    const roundedAmount = Math.round(purchaseAmount / 10) * 10;
-    return 100 - roundedAmount;
-};
-
-accountBalanceAfterPurchase(11);
